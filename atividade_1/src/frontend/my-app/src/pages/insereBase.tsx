e04e9e6404f0b9b2a0b75f865bb0c9266bb5fb8d
Edustn <eduardo.santos@sou.inteli.edu.br>
2024-09-07 22:47:57 -0300

    feat: insere frontend e suas confiugracoes

---
+++ atividade_1/src/frontend/my-app/src/pages/insereBase.tsx
@@ -0,0 +1,7 @@
+
+export default function InsereBase() {
+    return (
+      <div></div>
+    );
+  }
+  
---
+++ atividade_1/src/frontend/my-app/src/pages/pagina.tsx
@@ -0,0 +1,9 @@
+
+export default function Pagina() {
+    return (
+        <> 
+      Hello, Pagina!
+      </>
+    );
+  }
+  
