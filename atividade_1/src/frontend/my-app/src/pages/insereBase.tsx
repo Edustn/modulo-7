e2977e90f78f60ee918abedde094e6898fadd016
Edustn <eduardo.santos@sou.inteli.edu.br>
2024-09-09 21:16:37 -0300

    feat: insere funcionalidade para inserir nova base de dados:

--- atividade_1/src/frontend/my-app/src/pages/insereBase.tsx
+++ atividade_1/src/frontend/my-app/src/pages/insereBase.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import "../app/globals.css";
 
 export default function InsereBase() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = async(e) => {
+    const uploadedFile = e.target.files[0];
+    setFile(uploadedFile);
+    console.log("Arquivo selecionado:", uploadedFile);
+  };
+
+  const handleButtonClick = () => {
+    // Quando o botão for clicado, simula o clique no input file escondido
+    document.getElementById("fileInput").click();
+  };
+
   return (
-    <>
-      <button className="text-slate-950 text-2xl text-center font-bold bg-amber-200 rounded-lg p-2 w-auto ">
-        Enviar Tabela
+    <div className="bg-amber-50 min-h-screen flex flex-col items-center justify-center">
+      <input
+        id="fileInput"
+        type="file"
+        accept=".csv"
+        style={{ display: "none" }} // Esconde o input de arquivo
+        onChange={handleFileChange}
+      />
+      <button
+        className="text-slate-950 text-2xl text-center font-bold bg-amber-200 rounded-lg p-2 w-auto"
+        onClick={handleButtonClick}>
+        Enviar tabela
       </button>
 
-
-    </>
+      {file && <p className="text-black mt-4">Arquivo selecionado: {file.name}</p>}
+    </div>
   );
 }
--- atividade_1/src/frontend/my-app/src/pages/pagina.tsx
+++ atividade_1/src/frontend/my-app/src/pages/pagina.tsx
@@ -1,9 +1,8 @@
 
 export default function Pagina() {
-    return (
-        <> 
-      Hello, Pagina!
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <h1 className=" text-rose-500 bg-slate-700">Hello, Pagina!</h1>
+    </>
+  );
+}
