c2291579500ea55e20011726f828087c08d5e26d
Edustn <eduardo.santos@sou.inteli.edu.br>
2024-09-26 11:02:55 -0300

    feat: insere teste do docker

--- atividade_1/src/frontend/my-app/src/pages/insereBase.tsx
+++ atividade_1/src/frontend/my-app/src/pages/insereBase.tsx
@@ -1,45 +1,39 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import "../app/globals.css";
-
-import Link from 'next/link';
+// import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import axios from "axios";
-
-
 
 export default function InsereBase() {
 
   const router = useRouter();
+  
+  // Definir o tipo do arquivo com File | null para aceitar arquivos ou nulo
+  const [file, setFile] = useState<File | null>(null);
 
   const handleBaseTreinadaClick = async () => {
     try {
-
+      // Realizando a requisição usando axios
       const response = await axios.get('http://localhost:8000/executar/modelo');
-
       console.log('Resposta da API:', response.data);
-
-      router.push('/results')
-
+      // Redireciona para a página de resultados
+      router.push('/results');
     } catch (error) {
-      console.error('Erro na rota /executar/modelo', error)
+      console.error('Erro na rota /executar/modelo', error);
     }
+  };
 
-  }
-
-
-
-
-
-  const [file, setFile] = useState(null);
-
-  const handleFileChange = (e) => {
-    const uploadedFile = e.target.files[0];
-    setFile(uploadedFile);
-    console.log("Arquivo selecionado:", uploadedFile);
+  // Adicionar o tipo para o evento de mudança de arquivo
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const uploadedFile = e.target.files[0];
+      setFile(uploadedFile);
+      console.log("Arquivo selecionado:", uploadedFile);
+    }
   };
 
   const handleButtonClick = () => {
-    document.getElementById("fileInput").click();
+    document.getElementById("fileInput")?.click();
   };
 
   const handleUpload = async () => {
